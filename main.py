d344d51123d0defbcd57df68de938bb778af0c9e
digitaljosh <joshuajmarkus@gmail.com>
2018-02-07 09:53:46 -0800

    spoonaular API call for recipe search, passed to view

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
+#from rapidconnect import RapidConnect
+import requests
 #from flask_sqlalchemy import SQLAlchemy 
 
 #TODO break main apart into seperate files later
@@ -10,6 +12,50 @@ app = Flask(__name__)
 
 #app.config['SQLALCHEMY_ECHO'] = True 
 app.config['DEBUG'] = True
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def recipe_search():
+
+    if request.method == 'POST':
+        search_query = request.form['search']
+        search_query = search_query.replace(" ","+")
+        api = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?query="
+        url = api + search_query
+        headers={
+            "X-Mashape-Key": "2lZIhttKlzmshfcvdDIws3dS8XAfp1Z9kkVjsn6Y7YuGocYKNB",
+            "Accept": "application/json"
+            }
+
+        json_data = requests.get(url, headers=headers).json()
+        
+
+        print("########################################")
+        title = json_data['results'][0]['title']
+        print(title)
+        print(type(title))
+        print(type(json_data))
+
+
+        print("########################################")
+            
+        
+        ''' # making list of titles from json data
+        recipe_list = []
+        for recipe in json_data['results']:
+            recipe_list.append(recipe['title'])
+        '''
+
+        print("########################################")
+        #print(json_data)
+        print("########################################")
+
+
+        return render_template('search.html', recipe_list=json_data)
+
+    else:
+        return render_template('search.html')
+
 
 
 
