6afc7aaba31410671d1078e90bb0209a016ba490
JasonOnes <jasonr.jones14@gmail.com>
2018-02-09 14:01:57 -0800

    recipe db notes, hidden.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
 
- 
 app.secret_key = 'Shhhhh,dont_tell_anyone1979'
 
 app.config['SQLALCHEMY_ECHO'] = True
