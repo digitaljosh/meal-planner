0842fd6670970a3c44f548fc15ec28c0e0b129dc
JasonOnes <jasonr.jones14@gmail.com>
2018-02-09 10:23:41 -0800

    passwords hashed and salted

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from hidden import local_db_connect
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
 
- 
-app.secret_key = 'Shhhhh,dont_tell_anyone1979'
+app.config['SQLALCHEMY_DATABASE_URI'] = local_db_connect
+
+app.secret_key = 'amsdfsndfknasdlfknl'
+
 
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG'] = True
