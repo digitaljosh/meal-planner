1914c70d39bcf90c6001460f80f12bc4f479b125
JasonOnes <jasonr.jones14@gmail.com>
2018-03-27 18:50:25 -0700

    dumb error

--- hidden.py
+++ hidden.py
@@ -9,7 +9,7 @@ aws_db_connect = 'mysql+pymysql://jaques:t2H6U7gPsknkHLK3@planeater.crzzlspzikii
 
 heroku_db_connect = 'mysql+pymysql://b5cd42300cb6a5:f5bbcefc@us-cdbr-iron-east-05.cleardb.net/heroku_8efc64d03e23af1'
 
-DB_USERNAME = b5cd42300cb6a5:
-DB_PASSWORD = f5bbcefc@
-DB_HOST = us-cdbr-iron-east-05.cleardb.net/
-DB_DATABASE = heroku_8efc64d03e23af1
+# DB_USERNAME = b5cd42300cb6a5:
+# DB_PASSWORD = f5bbcefc@
+# DB_HOST = us-cdbr-iron-east-05.cleardb.net/
+# DB_DATABASE = heroku_8efc64d03e23af1
