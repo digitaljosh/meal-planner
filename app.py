cf41b3693d954d294c3f231ff73ed23b820746ff
JasonOnes <jasonr.jones14@gmail.com>
2018-03-27 18:54:34 -0700

    app/application

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 from hidden import heroku_db_connect
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-#application.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
 
 
-application.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
+app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 
-application.secret_key = 'Shhhhh,dont_tell_anyone1979'
+app.secret_key = 'Shhhhh,dont_tell_anyone1979'
 
-application.config['SQLALCHEMY_ECHO'] = True
-application.config['DEBUG'] = True
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG'] = True
 
-db = SQLAlchemy(application)
+db = SQLAlchemy(app)
 
