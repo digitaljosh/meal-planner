5543d53a3d4336e34f4ea8534d147b5a00f5c905
JasonOnes <jasonr.jones14@gmail.com>
2018-03-27 18:08:14 -0700

    pymysql

--- hidden.py
+++ hidden.py
@@ -7,7 +7,7 @@ josh_db_connect = 'mysql+pymysql://mealplanner:mealplanner@localhost:8889/mealpl
 
 aws_db_connect = 'mysql+pymysql://jaques:t2H6U7gPsknkHLK3@planeater.crzzlspzikii.us-west-2.rds.amazonaws.com:3306/PlanEAtdb'
 
-heroku_db_connect = 'mysql://b5cd42300cb6a5:f5bbcefc@us-cdbr-iron-east-05.cleardb.net/heroku_8efc64d03e23af1'
+heroku_db_connect = 'mysql+pymysql://b5cd42300cb6a5:f5bbcefc@us-cdbr-iron-east-05.cleardb.net/heroku_8efc64d03e23af1'
 
 DB_USERNAME = b5cd42300cb6a5:
 DB_PASSWORD = f5bbcefc@
