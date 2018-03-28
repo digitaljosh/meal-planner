9b341be99eb879abd7e4b34d9d7b34aa055ab533
JasonOnes <jasonr.jones14@gmail.com>
2018-03-27 18:05:22 -0700

    db, nltk

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from hidden import jason_db_connect, josh_db_connect, aws_db_connect
+from hidden import jason_db_connect, josh_db_connect, aws_db_connect, heroku_connect
 
 application = Flask(__name__)
 
@@ -10,7 +10,8 @@ application = Flask(__name__)
 # Comment out other devs' db's to run the application locally
 #application.config['SQLALCHEMY_DATABASE_URI'] = jason_db_connect
 #application.config['SQLALCHEMY_DATABASE_URI'] = josh_db_connect
-application.config['SQLALCHEMY_DATABASE_URI'] = aws_db_connect
+#application.config['SQLALCHEMY_DATABASE_URI'] = aws_db_connect
+application.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 
 application.secret_key = 'Shhhhh,dont_tell_anyone1979'
 
--- hidden.py
+++ hidden.py
@@ -6,3 +6,10 @@ jason_db_connect = 'mysql+pymysql://DinDin2:1meal@localhost:3306/DinDin2'
 josh_db_connect = 'mysql+pymysql://mealplanner:mealplanner@localhost:8889/mealplanner'
 
 aws_db_connect = 'mysql+pymysql://jaques:t2H6U7gPsknkHLK3@planeater.crzzlspzikii.us-west-2.rds.amazonaws.com:3306/PlanEAtdb'
+
+heroku_db_connect = 'mysql://b5cd42300cb6a5:f5bbcefc@us-cdbr-iron-east-05.cleardb.net/heroku_8efc64d03e23af1'
+
+DB_USERNAME = b5cd42300cb6a5:
+DB_PASSWORD = f5bbcefc@
+DB_HOST = us-cdbr-iron-east-05.cleardb.net/
+DB_DATABASE = heroku_8efc64d03e23af1
