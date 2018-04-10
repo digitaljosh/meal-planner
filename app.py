2770f8a754722082450db29ada908f9c5d8000c8
JasonOnes <jasonr.jones14@gmail.com>
2018-04-10 14:06:12 -0700

    config vars

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ import os
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
-
-heroku_db_connect = S3Connection(os.environ['DATABASE_URL'])
-shhh = S3Connection(os.environ['SECRET_KEY'])
+secret_conn = S3Connection(os.environ['S3_Key'], os.environ['S3_SECRET'])
+#heroku_db_connect = S3Connection(os.environ['DATABASE_URL'])
+#shhh = S3Connection(os.environ['SECRET_KEY'])
+heroku_db_connect = secret_conn(['DATABASE_URL'])
+shhh = secret_conn(['SECRET_KEY'])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
