a64e4cb181d2bf0e1c01c93ad471425e58ddcf27
JasonOnes <jasonr.jones14@gmail.com>
2018-04-10 14:27:44 -0700

    revert to hidden

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from boto.s3.connection import S3Connection
 
 import os 
-#from hidden import heroku_db_connect, shhh
+from hidden import heroku_db_connect, shhh
 
 
 app = Flask(__name__)
 
+'''
 #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
 secret_conn = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 #heroku_db_connect = S3Connection(os.environ['DATABASE_URL'])
@@ -18,7 +19,7 @@ print(secret_conn)
 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 heroku_db_connect = secret_conn(['DATABASE_URL'])
 shhh = secret_conn(['SECRET_KEY'])
-
+'''
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
