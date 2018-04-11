fc8e6170f8f6f70f1dcbd532833c0d18d3100914
digitaljosh <joshuajmarkus@gmail.com>
2018-04-10 21:58:11 -0700

    refactored create_engine() to handle connection timeouts

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+# addded the following line to handle connection timeouts with heroku
 from sqlalchemy import create_engine
 
 #from boto.s3.connection import S3Connection
@@ -13,9 +15,10 @@ app = Flask(__name__)
 # imports config vars from os (Heroku)
 shhh = os.environ.get('SECRET_KEY')
 
-heroku_db_connect = os.environ.get('DATABASE_URL')
+#heroku_db_connect = os.environ.get('DATABASE_URL')
 
-engine = create_engine(heroku_db_connect, pool_pre_ping=True)
+
+heroku_db_connect = create_engine(os.environ.get('DATABASE_URL'), pool_pre_ping=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
