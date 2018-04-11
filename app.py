be9655960603f1231acc5a2acfcb4fdd82dece98
digitaljosh <joshuajmarkus@gmail.com>
2018-04-10 22:29:01 -0700

    updated sqlalchemy_pool_recycle to 59 seconds

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 # addded the following line to handle connection timeouts with heroku
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 
 #from boto.s3.connection import S3Connection
 
@@ -15,13 +15,14 @@ app = Flask(__name__)
 # imports config vars from os (Heroku)
 shhh = os.environ.get('SECRET_KEY')
 
-#heroku_db_connect = os.environ.get('DATABASE_URL')
+heroku_db_connect = os.environ.get('DATABASE_URL')
 
-
-heroku_db_connect = create_engine(os.environ.get('DATABASE_URL'), pool_pre_ping=True)
+# addded the following line to handle connection timeouts with heroku
+#engine = create_engine(heroku_db_connect, pool_pre_ping=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 59
 
 app.secret_key = shhh
 
