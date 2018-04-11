62035a137ece31a190bc0e4528c52f35841f3a96
digitaljosh <joshuajmarkus@gmail.com>
2018-04-10 22:36:48 -0700

    deleted unnecessary create_engine()

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# addded the following line to handle connection timeouts with heroku
-#from sqlalchemy import create_engine
-
-#from boto.s3.connection import S3Connection
 
 import os 
 
@@ -17,8 +13,7 @@ shhh = os.environ.get('SECRET_KEY')
 
 heroku_db_connect = os.environ.get('DATABASE_URL')
 
-# addded the following line to handle connection timeouts with heroku
-#engine = create_engine(heroku_db_connect, pool_pre_ping=True)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_connect
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
